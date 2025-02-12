04d7df0bec8a96e0aa736a6d442b8cafc73d7695
Willi Kappler <willi.kappler@uni-tuebingen.de>
2025-02-12 19:02:21 +0100

    Add more code for simulation.

--- src/at_simulation.py
+++ src/at_simulation.py
@@ -3,6 +3,7 @@
 import timeit
 from datetime import timedelta
 import logging
+import math
 
 # External imports
 import numpy as np
@@ -10,6 +11,7 @@ import numpy as np
 # Local imports
 import mathieu_functions_OG as mf
 import at_config
+from at_element import ATElementType
 
 logger = logging.getLogger(__name__)
 
@@ -21,6 +23,22 @@ class ATSimulation:
 
     def run(self):
         start = timeit.default_timer()
+
+        alpha_t: float = self.config.alpha_t
+        alpha_l: float = self.config.alpha_l
+        beta: float = self.config.beta
+        gamma: float = self.config.gamma
+        ca: float = self.config.ca
+        num_cp: int = self.config.num_cp
+
+        for elem in self.config.elements:
+            match elem.kind:
+                case ATElementType.Circle:
+                    elem.calc_d_q(alpha_t, alpha_l, beta)
+                    elem.set_outline(num_cp)
+                    elem.calc_target(ca, beta, gamma)
+                case ATElementType.Line:
+                    pass
 
         self.solve_system()
 
@@ -39,13 +57,14 @@ class ATSimulation:
 
         # TODO: plot result
 
-    def conc_array(self, xmin: float, ymin: float, xmax: float, ymax: float, inc: float):
-        xaxis = np.arange(xmin, xmax, inc)
-        yaxis = np.arange(ymin, ymax, inc)
-        num_xaxis = len(xaxis)
-        num_yaxis = len(yaxis)
+    def conc_array(self, xmin: float, ymin: float, xmax: float, ymax: float,
+            inc: float) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+        xaxis: np.ndarray = np.arange(xmin, xmax, inc)
+        yaxis: np.ndarray = np.arange(ymin, ymax, inc)
+        num_xaxis: int = len(xaxis)
+        num_yaxis: int = len(yaxis)
 
-        result = np.zeros((num_xaxis, num_yaxis))
+        result: np.ndarray = np.zeros((num_xaxis, num_yaxis))
 
         for i in range(0, num_yaxis):
             for j in range(0, num_xaxis):
@@ -55,62 +74,89 @@ class ATSimulation:
         return (xaxis, yaxis, result)
 
     def calc_c(self, x: float, y: float) -> float:
+        for elem in self.config.elements:
+            if elem.is_inside(x, y):
+                return elem.c_inside()
+
+
         # TODO: implement
         return 0.0
 
-    def calc_uv(self, x: float, y: float, d: float):
+    def calc_uv(self, x: float, y: float, d: float) -> tuple[float, float]:
         alpha_l: float = self.config.alpha_l
         alpha_t: float = self.config.alpha_t
 
-        Y: float = np.sqrt(alpha_l / alpha_t) * y
+        Y: float = math.sqrt(alpha_l / alpha_t) * y
         B: float = x**2.0 + Y**2.0 - d**2.0
-        p: float = (-B + np.sqrt(B**2.0 + 4.0 * d**2.0 * x**2.0)) / (2.0 * d**2.0)
-        q: float = (-B - np.sqrt(B**2.0 + 4.0 * d**2.0 * x**2.0)) / (2.0 * d**2.0)
+        p: float = (-B + math.sqrt(B**2.0 + 4.0 * d**2.0 * x**2.0)) / (2.0 * d**2.0)
+        q: float = (-B - math.sqrt(B**2.0 + 4.0 * d**2.0 * x**2.0)) / (2.0 * d**2.0)
 
-        psi: float = np.nan
-        psi_0: float = np.arcsin(np.sqrt(p))
+        psi: float = math.nan
+        psi_0: float = math.asin(math.sqrt(p))
 
-        if Y >= 0 and x >= 0:
+        if Y >= 0.0 and x >= 0.0:
             psi = psi_0
-        if Y < 0 and x >= 0:
-            psi = np.pi - psi_0
-        if Y <= 0 and x < 0:
-            psi = np.pi + psi_0
-        if Y > 0 and x < 0:
-            psi = 2 * np.pi - psi_0
+        if Y < 0.0 and x >= 0.0:
+            psi = math.pi - psi_0
+        if Y <= 0.0 and x < 0.0:
+            psi = math.pi + psi_0
+        if Y > 0.0 and x < 0.0:
+            psi = 2.0 * math.pi - psi_0
 
-        eta: float = 0.5 * np.log(1 - 2 * q + 2 * np.sqrt(q**2 - q))
+        eta: float = 0.5 * math.log(1.0 - 2.0 * q + 2.0 * math.sqrt(q**2.0 - q))
         return (eta, psi)
 
-    def calc_mathieu(self, order: float, psi: float, eta: float, q: float):
-        m = mf.mathieu(q)
+    def calc_mathieu1(self, order: int, psi: float, eta: float,
+            q: float) -> np.ndarray:
+        m = mf.Mathieu(q)
+        Se = m.ce(order, psi).real
+        Ye = m.Ke(order, eta).real
+        return Se * Ye
+
+    def calc_mathieu2(self, order: int, psi: float, eta: float,
+            q: float) -> tuple[np.ndarray, np.ndarray]:
+        m = mf.Mathieu(q)
         Se = m.ce(order, psi).real
         So = m.se(order, psi).real
         Ye = m.Ke(order, eta).real
         Yo = m.Ko(order, eta).real
-        return (Se, So, Ye, Yo)
-
-    def calc_d_q(self, r: float):
-        alpha_l: float = self.config.alpha_l
-        alpha_t: float = self.config.alpha_t
-        beta = self.config.beta
-
-        d = np.sqrt((r * np.sqrt(alpha_l / alpha_t))**2.0 - r**2.0)
-        q = (d**2.0 * beta**2.0) / 4.0
-        return (d, q)
+        return (So * Yo, Se * Ye)
 
     def solve_system(self):
         # TODO: calculate
         ms_expansion = []
         boundary_cond = []
 
-        num_elems = len(self.config.elements)
+        elements = self.config.elements
+        num_elems = len(elements)
 
-        for i in range(0, num_elems):
-            for j in range(0, num_elems):
-                if i != j:
-                    # TODO: match on element kind
-                    pass
+        for e1 in range(0, num_elems):
+            elem1 = elements[e1]
+            elem1.m_list = []
+            for e2 in range(0, num_elems):
+                elem2 = elements[e2]
+                for i in range(0, self.config.num_cp):
+                    d = elem1.d
+                    q = elem1.q
+
+                    if e1 == e2:
+                        (x, y) = elem1.outline[i]
+                    else:
+                        (x1, y1) = elem1.outline[i]
+                        (x2, y2) = elem2.outline[i]
+                        dist_x = abs(x1 - x2)
+                        dist_y = abs(y1 - y2)
+                        x = x1 - dist_x
+                        y = y1 - dist_y
+
+                    (eta, psi) = self.calc_uv(x, y, d)
+                    elem1.m_list.append(self.calc_mathieu1(0, psi, eta, q))
+                    for j in range(1, self.config.num_terms):
+                        (f1, f2) = self.calc_mathieu2(j, psi, eta, q)
+                        elem1.m_list.append(f1)
+                        elem1.m_list.append(f2)
+
+
 
         # Solve least squares
         coeff = np.linalg.lstsq(ms_expansion, boundary_cond, rcond=None)
