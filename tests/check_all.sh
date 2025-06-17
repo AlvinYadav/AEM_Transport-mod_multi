84e0863f72545da03b01d4208b4ae9cb930fbc92
Willi Kappler <willi.kappler@uni-tuebingen.de>
2025-06-17 18:45:38 +0200

    Refactor: use bash functions.

--- tests/check_all.sh
+++ tests/check_all.sh
@@ -1,29 +1,40 @@
 #!/usr/bin/env bash
 
+run_ruff() {
+    echo "ruff:"
+    ruff check src/
+    ruff check tests/
+
+    echo -e "\n\n-------------------------------------------\n\n"
+}
+
+run_mypy() {
+    echo "mypy:"
+    mypy --check-untyped-defs src/
+    mypy --check-untyped-defs tests/
+
+    echo -e "\n\n-------------------------------------------\n\n"
+}
+
+run_flake8() {
+    echo "flake8:"
+    flake8 src/
+    flake8 tests/
+
+    echo -e "\n\n-------------------------------------------\n\n"
+}
+
+run_pyright() {
+    echo "pyright:"
+    pyright src/
+    pyright tests/
+}
+
 reset
 
 export PYTHONPATH=$PYTHONPATH:"src/"
 
-echo "ruff:"
-ruff check src/
-ruff check tests/
-
-echo -e "\n\n-------------------------------------------\n\n"
-
-echo "mypy:"
-mypy --check-untyped-defs src/
-mypy --check-untyped-defs tests/
-
-
-echo -e "\n\n-------------------------------------------\n\n"
-
-echo "flake8:"
-flake8 src/
-flake8 tests/
-
-echo -e "\n\n-------------------------------------------\n\n"
-
-echo "pyright:"
-pyright src/
-pyright tests/
-
+run_ruff
+run_mypy
+run_flake8
+run_pyright
